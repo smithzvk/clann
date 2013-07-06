e11f477d5dfc8df0df5732493ac92b8fc7c17be3
Zach Kost-Smith <zachkostsmith@gmail.com>
2013-07-06 18:27:40 -0500

    FORWARD-PROPAGATION will perform the forward propagation on a batch of inputs.

---
+++ forward-propagation.lisp
@@ -0,0 +1,19 @@
+
+(in-package :clann)
+
+(defun sigmoid (x) (/ (+ 1 (exp (- x)))))
+
+(defun forward-propagation (inputs network)
+  (let ((nl (network-list network))
+        (z* nil)
+        (a* (list inputs)))
+    (iter (for (th units) :in nl)
+      (setf z* (bm:* (unmap-into
+                      'array
+                      (append-imas
+                       (list (make-list (ima-dimension (first a*) 0)
+                                        :initial-element '(1))
+                             (first a*))
+                       1))
+                    (ima:transpose th))
+            a* (collecting (ima:map-ima 'sigmoid (unmap-into 'array z*)))))))
