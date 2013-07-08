872dcaea4e27a6d37dda930502935030f7c0a609
Zach Kost-Smith <zachkostsmith@gmail.com>
2013-07-07 21:01:03 -0500

    Prepend the inputs onto the activities.

--- forward-propagation.lisp
+++ forward-propagation.lisp
@@ -7,13 +7,15 @@
   (let ((nl (network-list network))
         (z* nil)
         (a* (list inputs)))
-    (iter (for (th units) :in nl)
-      (setf z* (bm:* (unmap-into
-                      'array
-                      (append-imas
-                       (list (make-list (ima-dimension (first a*) 0)
-                                        :initial-element '(1))
-                             (first a*))
-                       1))
-                    (ima:transpose th))
-            a* (collecting (ima:map-ima 'sigmoid (unmap-into 'array z*)))))))
+    (cons
+     (first a*)
+     (iter (for (th units) :in nl)
+       (setf z* (bm:* (unmap-into
+                       'array
+                       (append-imas
+                        (list (make-list (ima-dimension (first a*) 0)
+                                         :initial-element '(1))
+                              (first a*))
+                        1))
+                      (ima:transpose th))
+             a* (collecting (ima:map-ima 'sigmoid (unmap-into 'array z*))))))))
